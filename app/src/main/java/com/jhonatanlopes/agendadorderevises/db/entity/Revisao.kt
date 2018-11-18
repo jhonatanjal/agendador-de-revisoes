5ef44b53a85eb7f4a83d05876aad78ae12dcfad0
Jhonatan Lopes <jhonatan.jal@gmail.com>
2018-11-18 13:16:13 -0200

    ajustando layout para novo formato de data

--- app/src/main/java/com/jhonatanlopes/agendadorderevises/db/entity/Revisao.kt
+++ app/src/main/java/com/jhonatanlopes/agendadorderevises/db/entity/Revisao.kt
@@ -4,6 +4,7 @@ import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
+import com.jhonatanlopes.agendadorderevises.utilities.maisUmDia
 import java.util.*
 
 @Entity(tableName = "revisao")
@@ -18,5 +19,5 @@ class Revisao(
     constructor(
         materia: String,
         assunto: String
-    ) : this(null, materia, assunto, Calendar.getInstance().time, 0)
+    ) : this(null, materia, assunto, Calendar.getInstance().time.maisUmDia(), 1)
 }
